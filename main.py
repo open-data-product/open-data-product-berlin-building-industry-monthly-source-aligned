deda3d91597028cf4eb8154897d0ea9c7203fbfd
Florian Schwanz <florian.schwanz@interoberlin.de>
2023-11-14 07:22:24 +0100

    chore: add data extractor #1

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import getopt
+import os
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from lib.extract.data_extractor import extract_data
+from lib.tracking_decorator import TrackingDecorator
+
+file_path = os.path.realpath(__file__)
+script_path = os.path.dirname(file_path)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@TrackingDecorator.track_time
+def main(argv):
+    # Set default values
+    clean = False
+    quiet = False
+
+    # Read command line arguments
+    try:
+        opts, args = getopt.getopt(argv, "hcq", ["help", "clean", "quiet"])
+    except getopt.GetoptError:
+        print("main.py --help --clean --quiet")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print("main.py")
+            print("--help                           show this help")
+            print("--clean                          clean intermediate results before start")
+            print("--quiet                          do not log outputs")
+            sys.exit()
+        elif opt in ("-c", "--clean"):
+            clean = True
+        elif opt in ("-q", "--quiet"):
+            quiet = True
+
+    manifest_path = os.path.join(script_path, "data-product.yml")
+    raw_path = os.path.join(script_path, "raw")
+
+    #
+    # Extract
+    #
+
+    extract_data(manifest_path=manifest_path, results_path=raw_path, clean=clean, quiet=quiet)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main(sys.argv[1:])
